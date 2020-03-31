751f770e9500006ebd39f6bfbd1ee8e0d40134f0
Seth Morenos <sethvm64@gmail.com>
2020-03-31 18:47:40 -0400

    style widget

--- src/App.js
+++ src/App.js
@@ -10,18 +10,24 @@ import Logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-        <div className='App-header'>
+        <div className='row'>
             <Widget header='Polyglot Dash' icon={Logo}>
                 <p>A w.i.p. dashboard for data visualization</p>
             </Widget>
             <Widget header='Radial Chart' icon={Logo}>
-                <p>Lorem Ipsum Description</p>
+                <p>Data Description
+                    <br/>• Point 1
+                    <br/>• Point 2
+                    <br/>• Point 3
+                </p>
                 <DonutChart 
                 width={200}
                 height={200} />
             </Widget>
             <Widget header='Line Graph' icon={Logo}>
-                <p>Lorem Ipsum Description</p>
+                <p>Data Description
+                    <br/>Lorem Ipsum dolor sit amet
+                </p>
                 <LineChart 
                 width={200}
                 height={200} />

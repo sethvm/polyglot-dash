acd644ef7619ece3a7f8035a6ec6543d25d9577c
Seth Morenos <sethvm64@gmail.com>
2020-04-08 10:45:35 -0400

    merge grid item into widget component

--- src/Dash.js
+++ src/Dash.js
@@ -18,35 +18,30 @@ export default function Dash() {
     return(
         <div className={dash.root}>
             <Grid container>
-                <Grid item lg={2} md={3} sm={3} xs={12}>
-                    <Widget header='NUMBER COUNT'>
-                        <h3>27.8K</h3>
-                    </Widget>
-                </Grid>
-                <Grid item lg={10} md={9} sm={9} xs={12}>
-                    <Widget header='WIDGET'>
-                        <DonutChart />
-                        <DonutChart />
-                        <DonutChart />
-                    </Widget>
-                </Grid>
+                <Widget lg={2} md={3} sm={3} xs={12}
+                header='NUMBER COUNT'>
+                    <h3>27.8K</h3>
+                </Widget>
+                <Widget lg={10} md={9} sm={9} xs={12}
+                header='WIDGET'>
+                    <DonutChart />
+                    <DonutChart />
+                    <DonutChart />
+                </Widget>
             </Grid>
             <Grid container>
-                <Grid item lg={2} md={2} sm={3} xs={12}>
-                    <Widget header='NUMBER COUNT'>
-                        <h3>27.8K</h3>
-                    </Widget>
-                </Grid>
-                <Grid item lg={5} md={5} sm={4} xs={12}>
-                    <Widget header='WIDGET'>
-                        <DonutChart />
-                    </Widget>
-                </Grid>
-                <Grid item lg={5} md={5} sm={4} xs={12}>
-                    <Widget header='WIDGET'>
-                        <DonutChart />
-                    </Widget>
-                </Grid>
+                <Widget lg={2} md={2} sm={3} xs={12}
+                header='NUMBER COUNT'>
+                    <h3>27.8K</h3>
+                </Widget>
+                <Widget lg={5} md={5} sm={4} xs={12}
+                header='WIDGET'>
+                    <DonutChart />
+                </Widget>
+                <Widget lg={5} md={5} sm={4} xs={12}
+                header='WIDGET'>
+                    <DonutChart />
+                </Widget>
             </Grid>
         </div>
     );
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const widgetStyle = makeStyles({
-    root: {
+    card: {
         background: '#202a3b',
         borderRadius: '7px',
         margin: '10px',
@@ -26,15 +27,21 @@ const widgetStyle = makeStyles({
 export default function Widget(props) {
     const widget = widgetStyle();
     return(
-        <Card className={widget.root}>
-            <CardContent>
-                <Typography className={widget.label}>
-                    {props.header}
-                </Typography>
-                <Typography className={widget.content}>
-                    {props.children}
-                </Typography>
-            </CardContent>
-        </Card>
+        <Grid item 
+        lg={props.lg}
+        md={props.md}
+        sm={props.sm}
+        xs={props.xs}>
+            <Card className={widget.card}>
+                <CardContent>
+                    <Typography className={widget.label}>
+                        {props.header}
+                    </Typography>
+                    <Typography className={widget.content}>
+                        {props.children}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     );
 }
